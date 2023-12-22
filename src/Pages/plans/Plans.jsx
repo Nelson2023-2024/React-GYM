38691ec0561842032937590b4ee4f631ef49b508
Nelson Obuya Ouya <nelsonobuya19@gmail.com>
2023-12-22 11:10:00 +0300

    Added the JSX for the Plans section

--- src/Pages/plans/Plans.jsx
+++ src/Pages/plans/Plans.jsx
@@ -1,10 +1,55 @@
+
+//data js
+import { plans } from '../../data'
+
+//files
+import PrimaryHeader from '../../Components/PrimaryHeader'
+
+//css
 import './plans.css'
+
+//images
+import HeaderImage from '../../images/header_bg_3.jpg'
+
+
+//reusable componect
+import Card from '../../UI/Card'
 
 function Plans() {
   return (
-    <div>
-      Plans
-    </div>
+    <>
+    <PrimaryHeader title={'Membership Plans'} image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, veritatis illo. Sint, quos. Eum eos aliquid vel quia blanditiis consequatur vitae ad, veritatis dolorem officia adipisci, necessitatibus qui numquam illum.
+    </PrimaryHeader>
+
+    <section className="plans">
+      <div className="container plans__container">
+        {
+          plans.map(({id, name, desc, price, features}) =>{
+            return(
+              <Card className={'plan'} key={id}>
+                <h3>{name}</h3>
+                <small>{desc}</small>
+                <h1>{`$${price}`}</h1><h2>/month</h2>
+                <h4>Features</h4>
+                {
+                  features.map(({feature, available}, index) => {
+                    return(
+                      <p key={index} className={!available ? 'disabled' : ''}>
+                        {feature}
+                      </p>
+                    )
+                  } )
+                }
+                <button className='btn lg'>Choose Plan</button>
+              </Card>
+            )
+          })
+        }
+      </div>
+    </section>
+    
+    </>
   )
 }
 
