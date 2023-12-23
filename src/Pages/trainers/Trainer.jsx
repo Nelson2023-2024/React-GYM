c91d5bf4a488b378c7b6d5b52d15ff94aec72450
Nelson Obuya Ouya <nelsonobuya19@gmail.com>
2023-12-23 19:29:35 +0300

    Added the JSX, styling, mapped the trainers array and addede responsive design to trainers page

--- src/Pages/trainers/Trainer.jsx
+++ src/Pages/trainers/Trainer.jsx
@@ -1,10 +1,49 @@
 import './trainers.css'
 
+//reusable header
+import PrimaryHeader from '../../Components/PrimaryHeader'
+
+//images
+import HeaderImage from '../../images/header_bg_4.jpg'
+
+//data.js
+import { trainers } from '../../data'
+import Train from '../../Components/Train'
+
+//icons
+import { FaLinkedin } from "react-icons/fa6";
+import { FaFacebookF } from "react-icons/fa6";
+import { FaSnapchatGhost } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaXTwitter } from "react-icons/fa6";
+
+
+
 function Trainer() {
   return (
-    <div>
-      Trainers
-    </div>
+    <>
+    <PrimaryHeader title='Trainers' image={HeaderImage}>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui laudantium atque iste quis neque quo, laborum odit architecto a quam, at beatae illo ducimus expedita repudiandae corporis, aut earum quos.
+    </PrimaryHeader>
+
+    <section className="trainers">
+      <div className="container trainers__container">
+        {
+          trainers.map(({id, image, name, job, socials}) => {
+            return(
+              <Train key={id} image={image} name={name} job={job} socials={[
+                {icon: <FaInstagram/>, link:socials[0]},
+                {icon: <FaXTwitter/>, link:socials[1]},
+                {icon: <FaFacebookF/>, link:socials[2]},
+                {icon: <FaLinkedin/>, link:socials[3]},
+               
+              ]}/>
+            )
+          })
+        }
+      </div>
+    </section>
+    </>
   )
 }
 
